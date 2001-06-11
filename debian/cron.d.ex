aeb6a8aeaab4fe19b13db489385863b8dc210462
xtrm <xtrm>
2001-06-11 17:20:55 +0000

    NEW: for debian packaging

---
+++ debian/cron.d.ex
@@ -0,0 +1,4 @@
+#
+# Regular cron jobs for the ocamlsdl package
+#
+0 4	* * *	root	ocamlsdl_maintenance
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(ocamlsdl):needs=X11|text|vc|wm section=Apps/see-menu-manual\
+  title="ocamlsdl" command="/usr/bin/ocamlsdl"
