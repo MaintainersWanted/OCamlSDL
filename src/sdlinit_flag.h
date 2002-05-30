f947aa5aaac316ff680b8c977fe13507d6edd021
xtrm <xtrm>
2002-05-30 16:01:10 +0000

    NEW: varcc utility generated files

---
+++ src/sdlinit_flag.h
@@ -0,0 +1,14 @@
+/* init_flag : tags and macros */
+#define MLTAG_TIMER	Val_int(237238181)
+#define MLTAG_AUDIO	Val_int(628011190)
+#define MLTAG_VIDEO	Val_int(887770203)
+#define MLTAG_CDROM	Val_int(-1056317969)
+#define MLTAG_JOYSTICK	Val_int(-176721732)
+#define MLTAG_NOPARACHUTE	Val_int(516093760)
+#define MLTAG_EVENTTHREAD	Val_int(61759044)
+#define MLTAG_EVERYTHING	Val_int(283065971)
+
+extern lookup_info ml_table_init_flag[];
+#define Val_init_flag(data) ml_lookup_from_c (ml_table_init_flag, data)
+#define Init_flag_val(key) ml_lookup_to_c (ml_table_init_flag, key)
+
---
+++ src/sdlvideo_flag.h
@@ -0,0 +1,18 @@
+/* video_flag : tags and macros */
+#define MLTAG_SWSURFACE	Val_int(830934505)
+#define MLTAG_HWSURFACE	Val_int(556239582)
+#define MLTAG_SRCCOLORKEY	Val_int(-122876192)
+#define MLTAG_SRCALPHA	Val_int(-961997318)
+#define MLTAG_ASYNCBLIT	Val_int(-14289583)
+#define MLTAG_ANYFORMAT	Val_int(565310211)
+#define MLTAG_HWPALETTE	Val_int(809613100)
+#define MLTAG_DOUBLEBUF	Val_int(-786331486)
+#define MLTAG_FULLSCREEN	Val_int(-339890629)
+#define MLTAG_OPENGL	Val_int(-736685969)
+#define MLTAG_OPENGLBLIT	Val_int(708015140)
+#define MLTAG_RESIZABLE	Val_int(615032651)
+#define MLTAG_NOFRAME	Val_int(-970104788)
+
+extern lookup_info ml_table_video_flag[];
+#define Val_video_flag(data) ml_lookup_from_c (ml_table_video_flag, data)
+#define Video_flag_val(key) ml_lookup_to_c (ml_table_video_flag, key)
