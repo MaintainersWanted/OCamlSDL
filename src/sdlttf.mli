1e28e49fa67577c5652842cd9c24643a9566e9e9
xtrm <xtrm>
2002-08-08 15:44:08 +0000

    ADD: ocamldoc comments

--- src/sdlttf.mli
+++ src/sdlttf.mli
@@ -17,57 +17,82 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *)
 
-(* $Id: sdlttf.mli,v 1.7 2002/04/30 16:04:36 xtrm Exp $ *)
+(* $Id: sdlttf.mli,v 1.8 2002/08/08 15:44:08 xtrm Exp $ *)
+
+(** This module provides TTF (TrueType Font) support *)
 
 (* Exception *)
 
 exception SDLttf_exception of string
 
-(*1 Type *)
-
 type font
+(** abstract font datatype *)
 
-(*1 Operations *)
 
-val open_font : string -> int -> font
-(*d open a font file and create a font of the specified point size *)
+(** {1 General operations on font datatype} *)
 
-val close_font : font -> unit
-(*d close an opened font file *)
+external open_font : string -> int -> font = "sdlttf_open_font"
+(**   open a font file and create a font of the specified point size 
+  @return font datatype 
+*)
 
-val font_height : font -> int
-(*d get the total height of the font (usually equal to point size *)
+external close_font : font -> unit = "sdlttf_close_font"
+(**
+  close a previouly opened font file 
+  @param font a font datatype returned by open_font
+*)
 
-val font_ascent : font -> int
-(*d get the offset from the baseline to the top of the font
+external font_height : font -> int = "sdlttf_font_height"
+(**
+  @return the total height(int) of the font (usually equal to point size 
+*)
+
+external font_ascent : font -> int = "sdlttf_font_ascent"
+(**
+  @return the offset(int) from the baseline to the top of the font
     this is a positive value, relative to the baseline *)
 
-val font_descent : font -> int
-(*d get the offset from the baseline to the bottom of the font
-    this is a negative value, relative to the baseline *)
+external font_descent : font -> int = "sdlttf_font_descent"
+(**
+  @return the offset from the baseline to the bottom of the font
+  this is a negative value, relative to the baseline *)
 
-val font_metrics : font -> int -> int*int*int*int
-(*d get the metrics of the specified font *) 
+external font_metrics : font -> int -> (int*int*int*int) = "sdlttf_font_metrics"
+(**
+  @return the metrics (minx,maxx,miny,maxy) of the specified font 
+*) 
 
-(*1 Render text functions *)
+(** {1 Render text functions} *)
 
 
 (* render text functions *)
-val render_text_shaded : f:font -> text:string -> foreground:Sdlvideo.color -> background:Sdlvideo.color -> Sdlvideo.surface
-val render_text_blended : f:font -> text:string -> foreground:Sdlvideo.color -> Sdlvideo.surface
-val render_text_solid : f:font -> text:string -> foreground:Sdlvideo.color -> Sdlvideo.surface
+external render_text_shaded : f:font -> text:string -> foreground:Sdlvideo.color -> background:Sdlvideo.color -> Sdlvideo.surface = "sdlttf_render_text_shaded"
+(**  
+  @param f font datatype
+  @param text string to render
+  @param foreground the 'Sdlvideo.color' of the foreground
+  @param background the 'Sdlvideo.color' of the background use for the shaded fx
+  @return the surface where the shaded text is render
+  *)
+external render_text_blended : f:font -> text:string -> foreground:Sdlvideo.color -> Sdlvideo.surface = "sdlttf_render_text_blended"
+(**  
+  @param f font datatype
+  @param text string to render
+  @param foreground the 'Sdlvideo.color' of the foreground
+  @return the surface where the blended text is render
+  *)
 
-(*
-val render_text_shaded : f:font -> text:string -> foreground:(int * int * int) -> background:(int * int * int) -> Sdlvideo.surface
-val render_text_blended : f:font -> text:string -> foreground:(int * int * int) -> Sdlvideo.surface
-val render_text_solid : f:font -> text:string -> foreground:(int * int * int) -> Sdlvideo.surface
-*)
+external render_text_solid : f:font -> text:string -> foreground:Sdlvideo.color -> Sdlvideo.surface = "sdlttf_render_text_solid"
+(**  
+  @param f font datatype
+  @param text string to render
+  @param foreground the 'Sdlvideo.color' of the foreground
+  @return the surface where the text is render
+  *)
 
 (* deprecated *)
-val render_text : font -> string -> (int * int * int) -> (int * int * int) -> Sdlvideo.surface
+external render_text : font -> string -> (int*int*int) -> (int*int*int) -> Sdlvideo.surface = "sdlttf_render_text"
 
 (* Return a function to print strings, and another to clean up printer *)
-(*
-val make_printer : font -> (int * int * int) ->
-  (Sdlvideo.surface -> int -> int -> string -> unit) * (unit -> unit)
-*)
+(* val make_printer : font -> (int * int * int) ->
+  (Sdlvideo.surface -> int -> int -> string -> unit) * (unit -> unit) *)
