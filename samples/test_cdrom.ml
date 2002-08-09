3f404cd165a21d031794040277d985309c4d0d0d
oliv__a <oliv__a>
2002-08-09 13:46:22 +0000

    CHANGE: cdrom API (introduced cdrom_info type)

--- samples/test_cdrom.ml
+++ samples/test_cdrom.ml
@@ -3,44 +3,40 @@ open Sdl ;;
 open Sdlcdrom ;;
 
 (* init the SDL library *)
-init [`CDROM];; (*  init_with_auto_clean();; *)
+let _ =
+  init ~auto_clean:true [`CDROM];; 
 
 (* Get the number of cdrom drives *)
 let numbers_of_drives = get_num_drives ();;
 
-printf "Drives available: %d\n" numbers_of_drives ;;
+let _ =
+  printf "Drives available: %d\n" numbers_of_drives ;
 
-for id = 0 to (numbers_of_drives - 1 ) do
+  for id = 0 to (numbers_of_drives - 1 ) do
 
-  printf "Drive %d:  \"%s\"\n" id (drive_name id);
-  let cdrom = cd_open id
-  in  
-  let cdrom_state = cd_status cdrom
-  in 
-  let flag_track = ref 0
-  in let s = 
+    printf "Drive %d:  \"%s\"\n" id (drive_name id);
+    let cdrom = cd_open id in  
+    let cdrom_state = cd_status cdrom in 
+    let flag_track = ref 0 in 
+    let s = 
       match cdrom_state with 
-	  CD_TRAYEMPTY -> 
-	    "tray empty" 
-	| CD_STOPPED -> 
-	    "stopped"
-	| CD_PLAYING -> 
-	    flag_track := 1 ;
-	    "playing"
-	| CD_PAUSED -> 
-	    flag_track := 1 ;
-	    "paused"
-	| _ -> 
-	    "error state" 
-  in
-      printf "Drive %d status: %s\n" id s ;
-    if (!flag_track == 1) 
+	CD_TRAYEMPTY -> 
+	  "tray empty" 
+      | CD_STOPPED -> 
+	  "stopped"
+      | CD_PLAYING -> 
+	  flag_track := 1 ;
+	  "playing"
+      | CD_PAUSED -> 
+	  flag_track := 1 ;
+	  "paused"
+    in
+    printf "Drive %d status: %s\n" id s ;
+    if !flag_track = 1
     then
-      let current_track = (cd_current_track cdrom)
-      and (m,s) = (cd_track_current_time cdrom)
-      in
-	printf "Currently playing track %d, %2d:%2d\n" 
-	  (track_get_number current_track) m s;
-    cd_close cdrom 
-done 
-
+      let info = cd_info cdrom in
+      let (m, s, _) = msf_of_frames info.curr_frame in
+      printf "Currently playing track %d, %2d:%2d\n" 
+	info.curr_track m s ;
+      cd_close cdrom 
+  done 
