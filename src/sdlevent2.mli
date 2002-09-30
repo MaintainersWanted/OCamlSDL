a5eed0410dd53297bc7654629204366917dd4e0f
oliv__a <oliv__a>
2002-09-30 23:32:54 +0000

    ocamldoc comments

--- src/sdlevent2.mli
+++ src/sdlevent2.mli
@@ -17,116 +17,128 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *)
 
-(* $Id: sdlevent2.mli,v 1.4 2002/09/26 15:59:58 oliv__a Exp $ *)
+(* $Id: sdlevent2.mli,v 1.5 2002/09/30 23:32:54 oliv__a Exp $ *)
+
+(** SDL event handling *)
 
 exception Event_exn of string
+(** The exception used for reporting events-related errors. *)
 
+(** {1 Application focus} *)
+
+(** The available application states *)
 type active_state = 
-  | MOUSEFOCUS
-  | INPUTFOCUS
-  | ACTIVE
+  | MOUSEFOCUS  (** The app has mouse coverage *)
+  | INPUTFOCUS  (** The app has input focus *)
+  | ACTIVE      (** The application is active *)
 
-external get_app_state : unit -> active_state list
-    = "mlsdlevent_get_app_state"
+(** This function returns the current state of the application. If
+   ACTIVE is set, then the user is able to see your application,
+   otherwise it has been iconified or disabled. *)
+external get_app_state : unit -> active_state list =
+   "mlsdlevent_get_app_state"
 
+(** {1 Events datatypes} *)
+
+(** Application visibility event record *)
 type active_event = {
-    gain     : bool ;
-    ae_state : active_state list ;
+    gain     : bool ; (** Whether given states were gained or lost *)
+    ae_state : active_state list ; (** A list of the focus states *)
   } 
 
 type switch_state = 
   | RELEASED
   | PRESSED
 
+(** Keyboard event record *)
 type keyboard_event = {
-    ke_which : int ;
-    ke_state : switch_state ;
-    keysym   : Sdlkey.key ;
-    keymod   : Sdlkey.mod_state ;
-    keycode  : char ;
+    ke_which : int ;              (** The keyboard device index *)
+    ke_state : switch_state ;     (** PRESSED or RELEASED *)
+    keysym   : Sdlkey.key ;       (** SDL virtual keysym *)
+    keymod   : Sdlkey.mod_state ; (** current key modifiers *)
+    keycode  : char ;             (** translated character *)
   }
 
+(** Mouse motion event record *)
 type mousemotion_event = {
-    mme_which  : int ;
-    mme_state  : Sdlmouse.mouse_button list ;
-    mme_x    : int ;
+    mme_which  : int ;            (** The mouse device index *)
+    mme_state  : Sdlmouse.mouse_button list ; (** The current button state *)
+    mme_x    : int ;              (** The X/Y coordinates of the mouse *)
     mme_y    : int ;
-    mme_xrel : int ;
-    mme_yrel : int ;
+    mme_xrel : int ;              (** The relative motion in the X direction *)
+    mme_yrel : int ;              (** The relative motion in the Y direction *)
   } 
 
+(** Mouse button event record *)
 type mousebutton_event = {
-    mbe_which  : int ;
-    mbe_button : Sdlmouse.mouse_button ;
-    mbe_state  : switch_state ;
-    mbe_x : int ;
+    mbe_which  : int ;            (** The mouse device index *)
+    mbe_button : Sdlmouse.mouse_button ; (** The mouse button index *)
+    mbe_state  : switch_state ;   (** PRESSED or RELEASED *)
+    mbe_x : int ;                 (** The X/Y coordinates of the mouse at press time *)
     mbe_y : int ;
   } 
 
+(** Joystick axis motion event record *)
 type joyaxis_event = {
-    jae_which : int ;
-    jae_axis  : int ;
-    jae_value : int ;
+    jae_which : int ;             (** The joystick device index *)
+    jae_axis  : int ;             (** The joystick axis index *)
+    jae_value : int ;             (** The axis value (range: -32768 to 32767) *)
   } 
 
+(** Joystick axis motion event record *)
 type joyball_event = {
-    jle_which : int ;
-    jle_ball  : int ;
-    jle_xrel  : int ;
-    jle_yrel  : int ;
+    jle_which : int ;             (** The joystick device index *)
+    jle_ball  : int ;             (** The joystick trackball index *)
+    jle_xrel  : int ;             (** The relative motion in the X direction *)
+    jle_yrel  : int ;             (** The relative motion in the Y direction *)
   } 
 
+(** Joystick hat position change event record *)
 type joyhat_event = {
-    jhe_which : int ;
-    jhe_hat   : int ;
-    jhe_value : int ;
+    jhe_which : int ;             (** The joystick device index *)
+    jhe_hat   : int ;             (** The joystick hat index *)
+    jhe_value : int ;             (**  The hat position value: {v
+                          8   1   2
+                          7   0   3
+                          6   5   4 v}
+                                       Note that zero means the POV is centered. *)
   } 
 
+(** Joystick button event record *)
 type joybuton_event = {
-    jbe_which  : int ;
-    jbe_button : int ;
-    jbe_state  : switch_state ;
+    jbe_which  : int ;            (** The joystick device index *)
+    jbe_button : int ;            (** The joystick button index *)
+    jbe_state  : switch_state ;   (** PRESSED or RELEASED *)
   } 
 
+(** The main event type *)
 type event = 
-  | ACTIVE          of active_event
-  | KEYDOWN         of keyboard_event
-  | KEYUP           of keyboard_event
-  | MOUSEMOTION     of mousemotion_event
-  | MOUSEBUTTONDOWN of mousebutton_event
-  | MOUSEBUTTONUP   of mousebutton_event
-  | JOYAXISMOTION   of joyaxis_event
-  | JOYBALLMOTION   of joyball_event
-  | JOYHATMOTION    of joyhat_event
-  | JOYBUTTONDOWN   of joybuton_event
-  | JOYBUTTONUP     of joybuton_event
-  | QUIT
-  | SYSWM
-  | VIDEORESIZE     of int * int
-  | VIDEOEXPOSE
-  | USER            of int
+  | ACTIVE          of active_event      (** Application loses/gains visibility *)
+  | KEYDOWN         of keyboard_event    (** Keys pressed *)
+  | KEYUP           of keyboard_event    (** Keys released *)
+  | MOUSEMOTION     of mousemotion_event (** Mouse moved *)
+  | MOUSEBUTTONDOWN of mousebutton_event (** Mouse button pressed *)
+  | MOUSEBUTTONUP   of mousebutton_event (** Mouse button released *)
+  | JOYAXISMOTION   of joyaxis_event     (** Joystick axis motion *)
+  | JOYBALLMOTION   of joyball_event     (** Joystick trackball motion *)
+  | JOYHATMOTION    of joyhat_event      (** Joystick hat position change *)
+  | JOYBUTTONDOWN   of joybuton_event    (** Joystick button pressed *)
+  | JOYBUTTONUP     of joybuton_event    (** Joystick button released *)
+  | QUIT                                 (** User-requested quit *)
+  | SYSWM                                (** System specific event *)
+  | VIDEORESIZE     of int * int         (** User resized video mode *)
+  | VIDEOEXPOSE                          (** Screen needs to be redrawn *)
+  | USER            of int               (** for your use ! *)
 
 val string_of_event : event -> string
+(** Returns a short string descriptive of the event type, for debugging *)
 
-type event_kind =
-  | ACTIVE_EVENT 
-  | KEYDOWN_EVENT 
-  | KEYUP_EVENT 
-  | MOUSEMOTION_EVENT 
-  | MOUSEBUTTONDOWN_EVENT 
-  | MOUSEBUTTONUP_EVENT 
-  | JOYAXISMOTION_EVENT 
-  | JOYBALL_EVENT 
-  | JOYHAT_EVENT 
-  | JOYBUTTONDOWN_EVENT 
-  | JOYBUTTONUP_EVENT 
-  | QUIT_EVENT 
-  | SYSWM_EVENT 
-  | RESIZE_EVENT 
-  | EXPOSE_EVENT 
-  | USER_EVENT
+(** {1 Event masks } *)
 
 type event_mask = int
+
+(** Event masks values are ints and should be manipulated with [lor],
+   [land], etc. *)
 
 val active_mask          : event_mask
 val keydown_mask         : event_mask
@@ -150,30 +162,79 @@ val mouse_event_mask     : event_mask
 val joystick_event_mask  : event_mask
 val all_events_mask      : event_mask
 
+type event_kind =
+  | ACTIVE_EVENT 
+  | KEYDOWN_EVENT 
+  | KEYUP_EVENT 
+  | MOUSEMOTION_EVENT 
+  | MOUSEBUTTONDOWN_EVENT 
+  | MOUSEBUTTONUP_EVENT 
+  | JOYAXISMOTION_EVENT 
+  | JOYBALL_EVENT 
+  | JOYHAT_EVENT 
+  | JOYBUTTONDOWN_EVENT 
+  | JOYBUTTONUP_EVENT 
+  | QUIT_EVENT 
+  | SYSWM_EVENT 
+  | RESIZE_EVENT 
+  | EXPOSE_EVENT 
+  | USER_EVENT
+
 val make_mask : event_kind list -> event_mask
+val of_mask   : event_mask -> event_kind list
 
-val pump : unit -> unit
-
-external peek : ?mask:event_mask -> int -> event list = "mlsdlevent_peek"
-external get  : ?mask:event_mask -> int -> event list = "mlsdlevent_get"
-
-external add : event list -> unit = "mlsdlevent_add"
-
-external has_event : unit -> bool = "mlsdlevent_has_event"
-
-val poll : unit -> event option
-
-external wait : unit -> unit = "mlsdlevent_wait"
-val wait_event : unit -> event
-
-external get_state : event_kind -> bool = "mlsdlevent_get_state"
-external set_state : bool -> event_kind -> unit = "mlsdlevent_set_state"
+(** {1 Enabling/Disabling event collecting} *)
 
 val enable_events  : event_mask -> unit
+(** Specified events are collected and added to the event queue (when
+   [pump] is called). *)
+
 val disable_events : event_mask -> unit
+(** Specified events are not collected and won't appear in the event queue. *)
 
 external get_enabled_events : unit -> event_mask = "mlsdlevent_get_enabled"
+(** The mask of currently reported events. *)
 
-val of_mask : event_mask -> event_kind list
+external get_state : event_kind -> bool = "mlsdlevent_get_state"
+(** Query the reporting state of an event type. *)
 
+external set_state : bool -> event_kind -> unit = "mlsdlevent_set_state"
+(** Set the reporting state of one individual event type. *)
+
+
+(** {1 Handling events} *)
+
+val pump : unit -> unit
+(** Pumps the event loop, gathering events from the input devices.
+   This function updates the event queue and internal input device
+   state.  This should only be run in the thread that sets the video
+   mode. *)
+
+val wait_event : unit -> event
+(** Wait indefinitely for the next available event and return it. *)
+
+external wait : unit -> unit = "mlsdlevent_wait"
+(** Wait indefinitely for the next available event but leave it in the
+   queue. *)
+
+val poll : unit -> event option
+(** Poll for currently pending events and return one if available. *)
+
+external has_event : unit -> bool = "mlsdlevent_has_event"
+(** Poll for currently pending events and return [false] if the queue is empty. *)
+
+external peek : ?mask:event_mask -> int -> event list = "mlsdlevent_peek"
+(** Checks the event queue for messages : up to 'numevents' events at
+   the front of the event queue, matching 'mask', will be returned and
+   will not be removed from the queue. *)
+
+external get  : ?mask:event_mask -> int -> event list = "mlsdlevent_get"
+(** Checks the event queue for messages : up to 'numevents' events at
+   the front of the event queue, matching 'mask', will be returned and
+   will be removed from the queue. *)
+
+external add : event list -> unit = "mlsdlevent_add"
+(** Add events to the back of the event queue. *)
+
+(**/**)
 val link_me : unit
