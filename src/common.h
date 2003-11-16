11be85c122da9ad4456bf18318a7d0e3ddc04fef
oliv__a <oliv__a>
2003-11-16 14:38:43 +0000

    - use alloca instead of malloc/free for VLAs - clean up headers (all caml-related .h in common.h)

--- src/common.h
+++ src/common.h
@@ -17,15 +17,22 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-/* $Id: common.h,v 1.10 2002/11/21 11:01:18 oliv__a Exp $ */
+/* $Id: common.h,v 1.11 2003/11/16 14:38:43 oliv__a Exp $ */
 
 #ifndef _COMMON_H_
 #define _COMMON_H_
 
+#include "config.h"
+
 #include <caml/mlvalues.h>
+#include <caml/alloc.h>
+#include <caml/memory.h>
+#include <caml/callback.h>
+#include <caml/fail.h>
+#include <caml/bigarray.h>
+#include <caml/custom.h>
 
 /*
-
   Caml list manipulations
 
   Grabbed in ocamlsdl-0.3/sdl_stub.c 1.8 (2000/09/25)
@@ -37,7 +44,7 @@ extern value cons(value x,value l);
 #define is_nil     Is_long
 #define is_not_nil Is_block
 #define hd(v)      Field((v), 0)
-#define tl(v)      Field((v) ,1)
+#define tl(v)      Field((v), 1)
 extern int list_length(value l);
 
 
@@ -83,5 +90,21 @@ CAMLprim value ml_##cname (value arg1, value arg2) { return conv (cname (conv1 (
 void enter_blocking_section(void);
 void leave_blocking_section(void);
 
+/* 
+ * VLA / alloca stuff
+ */
+
+#ifdef __GNUC__
+# define LOCALARRAY(type, x, len)  type x [(len)]
+#else
+# ifdef HAVE_ALLOCA
+#  ifdef HAVE_ALLOCA_H
+#   include <alloca.h>
+#  endif
+#  define LOCALARRAY(type, x, len) type *x = (type *) alloca(sizeof (type) * (len))
+# else
+#  error "no alloca available"
+# endif
+#endif
 
 #endif /* _COMMON_H_ */
